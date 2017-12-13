a3b2f2c2c6ae4cac5a16cf558e423d58a1bf14c6
Ray Gomez <codenomad@gmail.com>
2017-12-13 01:52:57 -0700

    Cleanup gtk, added kivy

---
+++ kgui.py
@@ -0,0 +1,39 @@
+from kivy.app import App
+from kivy.clock import Clock
+from kivy.properties import BooleanProperty
+
+from kivy.uix.tabbedpanel import TabbedPanel
+from kivy.uix.togglebutton import ToggleButton
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.popup import Popup
+from kivy.uix.button import Button
+from kivy.uix.checkbox import CheckBox
+
+# Disabled imports
+# from kivy.uix.button import Label
+
+
+class HintAddButton(Button):
+    """ The Add Hint Button, this is required to enable/disable it"""
+    enabled = BooleanProperty(True)
+
+
+class AddHintPopup(Popup):
+    """ Hint Wizard """
+
+
+class RLayout(GridLayout):
+    """ Main Regioneer application Layout """
+
+    def add_hint_popup(self):
+        p = AddHintPopup()
+        p.open()
+
+
+class RegioneerApp(App):
+    def build(self):
+        return RLayout()
+
+
+if __name__ == "__main__":
+    RegioneerApp().run()
